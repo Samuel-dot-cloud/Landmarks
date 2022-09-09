c635c2c21550473f37ca80b65deff7debc1ad641
Samuel-dot-cloud <samuelwahome.k5@gmail.com>
2022-09-10 01:39:35 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Samuel Wahome on 10/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
